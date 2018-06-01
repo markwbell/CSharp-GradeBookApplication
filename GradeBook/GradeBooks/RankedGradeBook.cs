d6e6917f78b124f83916f321a4754d12cca0003b
Mark Bell <markwbell@live.com>
2018-06-01 08:19:29 -0400

    Implemented strange RankedGradeBook.GetLetterGrade  logic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException($"There must be at least 5 students for ranked grading. Count: {Students.Count}");
+
+            var studentsPerRank = Math.Round(Students.Count * .20);
+            var sortedStudents = Students.OrderByDescending(s => s.AverageGrade);
+            int studentCount = 0;
+            foreach (var student in sortedStudents)
+            {
+                if (averageGrade == student.AverageGrade)
+                {
+                    if (studentCount / studentsPerRank < 1) return 'A';
+                    if (studentCount / studentsPerRank < 2) return 'B';
+                    if (studentCount / studentsPerRank < 3) return 'C';
+                    if (studentCount / studentsPerRank < 4) return 'D';
+                    else return 'F';
+                }
+                studentCount++;
+            }
+
+            return 'F';
         }
     }
 }
